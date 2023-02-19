58e57c8faa65e78d894f459dc935d39c13fb4b4a
Carol <cnassuna@gmail.com>
2023-02-19 22:49:11 +0000

    edited file

--- sonarqube.sh
+++ sonarqube.sh
@@ -10,3 +10,7 @@ sudo unzip /opt/sonarqube-9.3.0.51899.zip
 sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
 cd /opt/sonarqube-x.x/bin/linux-x86-64
 ./sonar.sh start
+# Connect to the SonarQube server through the browser. It uses port 9000
+# http://<your-ip-address>:9000
+# sudo firewall-cmd --permanent --add-port=9000/tcpcd
+# sudo firewall-cmd --reload
